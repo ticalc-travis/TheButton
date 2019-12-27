d69cfe1f02e1f7a705a562d24d2b98dd7ddec617
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2019-12-27 16:20:31 -0500

    Add plugin for checking the time of a user.
    Usage: "!time <username>" or "!time" to check your own time.

--- js/iChat-plugins.js
+++ js/iChat-plugins.js
@@ -24,6 +24,49 @@ var pm = new iChatPlugin("action/pm", function (data) {
   return data;
 }, "Written by _iPhoenix_");
 
+const time = new iChatPlugin("thebutton/time", function(data) {
+  let txt = data.txt.trim();
+  if (txt.startsWith("!time")) {
+    let user = "";
+    if (txt == "!time") {
+      user = data.u;
+    } else {
+      user = txt.split(" ")[1].match(/(\w{2,32})/)[1];
+    }
+    if (user.length) {
+      function format(n) {
+        if (n) {
+          var r = n / 1e3,
+            t = r / 60,
+            o = t / 60,
+            e = o / 24;
+
+          function u(n, r) {
+            return (n = Math.floor(n)) + " " + r + (1 == n ? "" : "s") + ", ";
+          }
+          return t %= 60, o %= 24, r = u(r %= 60, "second"), t = u(t, "minute"), o = u(o, "hour"), (e = u(e, "day")) + o + t + "and " + (r = r.substring(0, r.length - 2));
+        }
+        return "No time.";
+      }
+
+      firebase.database().ref("/button/users/" + user).once("value", function(snapshot) {
+        let id = iChat.renderMessage({
+          ts: Date.now(),
+          u: "TheButton",
+          txt: user + " has " + format(snapshot.val())
+        });
+
+        firebase.database().ref("/button/users/" + user).on("value", function(snapshot) {
+          iChat.updateMessage(id, {
+            "txt": user + " has " + format(snapshot.val())
+          });
+        });
+      });
+    }
+  }
+  return data;
+}, "Written by _iPhoenix_");
+
 iChat.onload = function () {
-  iChat.registerPlugins(me, pm);
+  iChat.registerPlugins(me, pm, time);
 }
